f8611379ffcef7812615a27cc1c31ec42fd5edfb
afzsilva <afzsilva@gmail.com>
2025-05-19 19:11:26 -0300

    Initial commit

---
+++ src/main/java/com/afzdev/demo/livraria/DemoLivrariaApplication.java
@@ -0,0 +1,13 @@
+package com.afzdev.demo.livraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLivrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLivrariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afzdev/demo/livraria/DemoLivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afzdev.demo.livraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
