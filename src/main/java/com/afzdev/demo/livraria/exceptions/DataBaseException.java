06e95430c137bc4656cca18439ce24f59e43d9ff
afzsilva <afzsilva@gmail.com>
2025-06-04 13:22:14 -0300

    Adiciona exception handler e exception customizada

---
+++ src/main/java/com/afzdev/demo/livraria/exceptions/DataBaseException.java
@@ -0,0 +1,7 @@
+package com.afzdev.demo.livraria.exceptions;
+
+public class DataBaseException extends RuntimeException{
+    public DataBaseException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/afzdev/demo/livraria/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.afzdev.demo.livraria.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<ProblemDetail> SqlExceptionHelperDuplicateEntry (DataBaseException e, WebRequest request){
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getLocalizedMessage());
+        problemDetail.setTitle("Database Exception");
+        problemDetail.setProperty("Hora da ocorrencia", Instant.now());
+        return ResponseEntity.ok(problemDetail);
+    }
+
+
+}
