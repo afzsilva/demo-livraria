0452962434a1d966fc1ffb63874c21e62ff14b8f
afzsilva <afzsilva@gmail.com>
2025-06-04 15:45:07 -0300

    AJusta exceptions

--- src/main/java/com/afzdev/demo/livraria/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/afzdev/demo/livraria/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.afzdev.demo.livraria.exceptions;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
@@ -12,19 +13,19 @@ import java.time.Instant;
 @RestControllerAdvice
 public class ResourceExceptionHandler {
 
-    @ExceptionHandler(DataBaseException.class)
-    public ResponseEntity<ProblemDetail> SqlExceptionHelperDuplicateEntry (DataBaseException e, WebRequest request){
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.getLocalizedMessage());
-        problemDetail.setTitle("Database Exception");
-        problemDetail.setProperty("Hora da ocorrencia", Instant.now());
-        return ResponseEntity.ok(problemDetail);
-    }
-
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ProblemDetail> entityNotFound(ResourceNotFoundException e, WebRequest request){
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getLocalizedMessage());
         problemDetail.setTitle("Registro Inexistente");
         problemDetail.setProperty("Hora da ocorrencia", Instant.now());
-        return ResponseEntity.ok(problemDetail);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemDetail);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ProblemDetail> handleDataIntegrityViolation(DataIntegrityViolationException e, WebRequest request){
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, "Violação de integridade: dado duplicado ou inválido.");
+        problemDetail.setTitle("Erro de integridade no banco de dados");
+        problemDetail.setProperty("Hora da ocorrência", Instant.now());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(problemDetail);
     }
 }
